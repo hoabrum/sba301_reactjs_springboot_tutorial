3eeed826267da69423679db5f0b8e927ea66c735
Bui Hoa <hoabrum@gmail.com>
2026-09-23 17:01:16 +0700

    Test Branch Development

---
+++ reactjs-tutorial/07-react-demo-lab02/react-app-lab02/src/components/DemoComponent.jsx
@@ -0,0 +1,8 @@
+export default function DemoComponent() {
+   return (
+      <div>
+         <h1>Welcome to the Demo Component!</h1>
+         <p>This is a simple React component for demonstration purposes.</p>
+      </div>
+   );
+}
