5bde6b9f5f132515b512531094ee647247cca5c9
Bui Hoa <hoabrum@gmail.com>
2026-09-23 17:13:04 +0700

    Modified

--- reactjs-tutorial/07-react-demo-lab02/react-app-lab02/src/components/DemoComponent.jsx
+++ reactjs-tutorial/07-react-demo-lab02/react-app-lab02/src/components/DemoComponent.jsx
@@ -3,6 +3,7 @@ export default function DemoComponent() {
       <div>
          <h1>Welcome to the Demo Component!</h1>
          <p>This is a simple React component for demonstration purposes.</p>
+         <p>Feel free to customize it and add your own content.</p>
       </div>
    );
 }
